25550da62e9c259be247a9a6099f278684ad054e
Carlos Chitty <chitty.carlosj@gmail.com>
2016-12-09 16:48:19 -0500

    Get configuration from the environment

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,13 +1,20 @@
 import base64
 import datetime
 import hashlib
-import random
+import os
 import requests
 import sha
 import uuid
 
-API_KEY = 'MyS3cr3TAP1K3Y'
-USERNAME = 'myusername'
+API_KEY = os.environ.get('VM_API_KEY')
+USERNAME = os.environ.get('VM_USERNAME')
+
+if not API_KEY:
+    API_KEY = 'MyS3cr3TAP1K3Y'
+    print('WARNING: VM_API_KEY not set in the environment, using default.')
+if not USERNAME:
+    USERNAME = 'myusername'
+    print('WARNING: VM_USERNAME not set in the environment, using default.')
 
 nonce = base64.b64encode(sha.new(uuid.uuid4().hex).digest())
 created = '{:%Y-%m-%dT%H:%M:%SZ}'.format(datetime.datetime.now())
